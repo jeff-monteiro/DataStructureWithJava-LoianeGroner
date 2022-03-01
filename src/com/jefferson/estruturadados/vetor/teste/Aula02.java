f46c2b8a6df45331ffe7085afa1a83f88979b216
Jefferson Monteiro <jeffmonteiro61@icloud.com>
2022-03-01 14:13:40 -0300

    chore: structList

---
+++ src/com/jefferson/estruturadados/vetor/teste/Aula02.java
@@ -0,0 +1,13 @@
+package com.jefferson.estruturadados.vetor.teste;
+
+import com.jefferson.estruturadados.vetor.Vetor;
+
+public class Aula02 {
+    public static void main(String[] args){
+        Vetor vetor = new Vetor(5);
+
+        int i = 0;
+        i++;
+
+    }
+}
